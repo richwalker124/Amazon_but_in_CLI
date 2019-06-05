6b87eed50085ffcba07794a7c7e095da11525ba1
Rich <rjw12498@gmail.com>
2019-06-05 13:15:33 -0600

    added mysql functionality

--- js.js
+++ js.js
@@ -4,3 +4,21 @@
 //Design a small ui so user can see what is being manipulated.
 //Add in data, through either SQL or javascript, or even a third file.
  
+var mysql = require("mysql")
+
+var inquire = require("inquirer")
+
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    port : 3669,
+
+    password : "Dcsd138409",
+    database : "amazon"
+})
+
+
+connection.connect(function(err){
+    if (err) throw err
+    console.log(`connected to database as connection ${connection.threadId}\n`)
+})
